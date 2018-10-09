ae81df9860eb93219eedfe35747149c280f4a468
Arsalan-Syed <arsalan.s96@gmail.com>
2018-10-09 16:07:10 +0200

    Added encoding function to turn board representation into string

--- src/local_search.py
+++ src/local_search.py
@@ -1,33 +1,36 @@
-'''
+"""
 Created by Arsalan Syed on 9th October 2018
-'''
+"""
 
 MIN_VALUE = float("-inf")
 
+
 # TODO
 def get_neighbours(node):
-	return None
-	
+    return None
+
+
 # TODO	
 def evaluation(node):
-	return 0
+    return 0
+
 
 # TODO
 def hill_climb(startNode):
-	currentNode = startNode
-	maxIterations = 1000
-	iter = 0
-	
-	while(iter < maxIterations): 
-		bestEvaluation = MIN_VALUE
-		bestNode = None
-		for neighbour in get_neighbours(currentNode):
-			neighbourEvaluation = evaluation(neighbour)
-			if neighbourEvaluation > bestEvaluation:
-				bestNode = neighbour
-				bestEvaluation = neighbourEvaluation
-		
-		if bestEvaluation <= evaluation(currentNode):
-			return currentNode
-		
-		currentNode = nextEval
+    currentNode = startNode
+    maxIterations = 1000
+    iter = 0
+
+    while (iter < maxIterations):
+        bestEvaluation = MIN_VALUE
+        bestNode = None
+        for neighbour in get_neighbours(currentNode):
+            neighbourEvaluation = evaluation(neighbour)
+            if neighbourEvaluation > bestEvaluation:
+                bestNode = neighbour
+                bestEvaluation = neighbourEvaluation
+
+        if bestEvaluation <= evaluation(currentNode):
+            return currentNode
+
+        currentNode = bestNode
