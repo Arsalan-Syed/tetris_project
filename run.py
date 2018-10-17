ab09e55a004575192861b8fb828c1a3d7b60086b
Arsalan-Syed <arsalan.s96@gmail.com>
2018-10-17 14:53:10 +0200

    Added methods to run.py to try out different scenarios

--- run.py
+++ run.py
@@ -2,18 +2,27 @@
 
 import src.environment as env
 import src.filehandler as fh
+import src.heuristic as heur
+
+
+def runInfinitely(weights=heur.defaultWeights):
+    useGUI = True
+    App = env.TetrisApp(useGUI,weights)
+    App.run()
+
+
+def runSequence(sequenceNumber, useGUI, weights=heur.defaultWeights):
+    App = env.TetrisApp(useGUI, weights)
+    sequences = fh.loadSequences("sequences/test1.txt")
+    print(App.runSequence(sequenceNumber))
+
 
 def main():
-    random = False
     useGUI = True
 
-    App = env.TetrisApp(useGUI)
-
-    if random:
-        App.run()
-    else:
-        sequences = fh.loadSequences("sequences/test1.txt")
-        print(App.runSequence(sequences[0]))
+    # runInfinitely()
+    # runSequence(0,useGUI)
+    # runSequence(1,useGUI)
 
 
 if __name__ == '__main__':
