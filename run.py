874dd3c80aac033a7126fbbe1b0e0acf1684f61a
John Wikman <john.wikman@hotmail.com>
2018-10-17 16:26:37 +0200

    Added an interactive prompt to run.py and updated README with instructions for running the program.

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import sys
+
 import src.environment as env
 import src.filehandler as fh
 import src.heuristic as heur
@@ -20,9 +22,23 @@ def runSequence(sequenceNumber, useGUI, weights=heur.defaultWeights):
 def main():
     useGUI = True
 
-    # runInfinitely()
-    # runSequence(0,useGUI)
-    # runSequence(1,useGUI)
+    print("Choose an option: ")
+    print(" 1) run infinitely")
+    print(" 2) run sequence 0")
+    print(" 3) run sequence 1")
+    if sys.version_info > (3, 0):
+        choice = input("> ")
+    else:
+        choice = raw_input("> ")
+    choice = choice.strip()
+    if choice == "1":
+        runInfinitely()
+    elif choice == "2":
+        runSequence(0,useGUI)
+    elif choice == "3":
+        runSequence(1,useGUI)
+    else:
+        print("Invalid option. Input must be either 1, 2 or 3.")
 
 
 if __name__ == '__main__':
